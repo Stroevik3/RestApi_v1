72422826a8f0a441fc99eb02ff87b893844586f9
Konstantin Ivanov <wctft33k@gmail.com>
2023-03-11 18:38:14 +0500

    Lessen4

---
+++ internal/app/store/repository.go
@@ -0,0 +1,8 @@
+package store
+
+import "github.com/Stroevik3/RestApi_v1/internal/app/model"
+
+type UserRepository interface {
+	Create(*model.User) error
+	FindByEmail(string) (*model.User, error)
+}
